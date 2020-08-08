37e00e31e5f65ddac7c2cf47a0a1380b16cdb278
galoishelley <gracemiao2017@gmail.com>
2020-08-08 18:37:42 +1000

    bugfixeds

--- wp-content/themes/legpress/partials/modules/contact.php
+++ wp-content/themes/legpress/partials/modules/contact.php
@@ -3,13 +3,13 @@ $googlemap = get_sub_field('googlemap');
 $title = get_sub_field('title');
 
  ?>
-<section class="module module__contact" id="contact">
+<section class="module module__contact">
 	<div class="container">
 		<div class="row">
 			<div class="module__contact__image col-12 col-lg-6">
 			<div id="map"></div>
 			</div>
-			<div class="module__contact__copy col-12 col-lg-6">
+			<div class="module__contact__copy col-12 col-lg-6" id="contact">
 				<div class="module__contact__copy__inner typography">
 					<h1><?php echo $title; ?></h1>
 					<?php if ($form_shortcode = get_sub_field('ninja_form_shortcode')) { ?>
